1649e9455b5be77dd359e442e35ff67c6c569a96
NathanaelGracedo <nathanjuan56@gmail.com>
2025-02-18 10:03:33 +0700

    PWL Jobsheet 2 - Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function(){ 
+    return 'Hello World!';
+});
+
+Route::get('/world', function() {
+    return 'World!';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selama Datang';
+});
+
+Route::get('/about', function() {
+    return '2341720217 - Nathanael Juan Gracedo';
+});
+
+Route::get('/user/{name?}', function($name='john') {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'.$postId.' Komentar ke-: '.$commentId;
+});
+
+Route::get('/articles/{id}', function($articleId) {
+    return 'Halaman Artikel dengan ID '.$articleId;
 });
